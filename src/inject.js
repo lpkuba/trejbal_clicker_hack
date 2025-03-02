55d98e932cc70311b03f6bbff554fa53276953b8
lpkuba <kubmastr088@gmail.com>
2025-03-02 18:19:17 +0100

    hotfix 0.0.1a

--- src/inject.js
+++ src/inject.js
@@ -3,6 +3,7 @@ alert("Injected!");
 window.addEventListener("message", (event) =>{
     if(event.data.type !== "FROM_EXTENSION"){return;}
     if(confirm("Přidat?:" + event.data.amount)){
+        
         count += event.data.amount;
     };
 });
