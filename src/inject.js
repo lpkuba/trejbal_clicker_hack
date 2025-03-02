b088b4f5937b67df9e1d878c142abbe53000f40b
lpkuba <kubmastr088@gmail.com>
2025-03-02 23:38:55 +0100

    hotfix 0.0.1c

--- src/inject.js
+++ src/inject.js
@@ -1,14 +1,30 @@
+
+
+Object.defineProperty(performance, "now", {
+    value: () => 0,
+    writable: false
+});//skoro patch detekce devtools (neháže chybu už😜)
+ 
+
+
 alert("Injected!");
 
 window.addEventListener("message", (event) =>{
     if(event.data.type !== "FROM_EXTENSION"){return;}
     if(confirm("Přidat?:" + event.data.amount)){
-        let temp = clickValue;
-        clickValue = 1000000000;
+        const temp = clickValue;
+        const temp2 = rebirths;
+        clickValue = event.data.amount * 1.1;
+        rebirths += event.data.amount;
+        count += event.data.amount;
         setTimeout(() => {
-            count += event.data.amount;
+            alert("Skončil timeout negři!!!");
+            alert("Vracím hodnoty: \n Hodnota kliknutí z: " + clickValue + " na " + temp + "\nA rebirthy z: " + rebirths + " na " + temp2);
             clickValue = temp;
-        }, 3200);
-        
+            rebirths = temp2;
+        }, 4000);
+        alert("To už jsme za timeoutem??? MRDAT HO DO PÍČI!!!");
+
+
     };
 });
