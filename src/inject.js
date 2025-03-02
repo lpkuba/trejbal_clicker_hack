8004eabed180fab40028f86c971dd0510fd8d72d
lpkuba <kubmastr088@gmail.com>
2025-03-02 18:33:22 +0100

    patch 0.0.1b

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,12 @@ alert("Injected!");
 window.addEventListener("message", (event) =>{
     if(event.data.type !== "FROM_EXTENSION"){return;}
     if(confirm("Přidat?:" + event.data.amount)){
+        let temp = clickValue;
+        clickValue = 1000000000;
+        setTimeout(() => {
+            count += event.data.amount;
+            clickValue = temp;
+        }, 3200);
         
-        count += event.data.amount;
     };
 });
